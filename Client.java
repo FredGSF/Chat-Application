076fc00304ac25be7ee8a3f6fbd05d253d8e1d9a
Frederico Fernandes <frederico.garcia.2000@gmail.com>
2024-05-06 10:53:09 +0100

    fixing tabs

--- Client.java
+++ Client.java
@@ -1,6 +1,6 @@
 import java.io.*; 
 import java.net.*; 
-import java.util.Scanner;  
+import java.util.Scanner;   
 
 public class Client { 
 	private static final String SERVER_ADDRESS = "localhost"; 
